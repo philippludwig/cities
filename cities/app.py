c32bece15b988164f37302b74f7a7c9776e023b5
Philipp Ludwig <git@philippludwig.net>
2020-12-20 10:48:02 +0100

    Allow PUT and all headers.
    Without allowing all headers, the PUT requests fails.
    (Of course it would be better to figure out which exact headers
    are causing this problem)

--- cities/app.py
+++ cities/app.py
@@ -3,7 +3,7 @@ import falcon
 from .resources import CitiesResource, CityResource
 from falcon_cors import CORS
 
-cors = CORS(allow_all_origins=True)
+cors = CORS(allow_all_origins=True, allow_methods_list=["GET", "PUT"], allow_all_headers=True)
 api = application = falcon.API(middleware=[cors.middleware])
 
 api.add_route('/cities', CitiesResource())
